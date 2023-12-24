85c0905a21bd34c3a1289b329f5e9427688d8be1
anuj206 <y.206anuj@gmail.com>
2023-12-25 00:10:59 +0530

    DB added

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+"""
+Calculator Function
+"""
+
+def add(x,y):
+    """
+    Add x and y andd return the results.
+    """
+    return x+y
+
+def subtract(x,y):
+    """
+    Subtract x and y andd return the results.
+    """
+    return y - x 
+    
